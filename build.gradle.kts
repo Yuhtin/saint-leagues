12efde6ce0740c2cb050810e8039a11a1855c0c1
Davi Duarte <rio.daviduarte@gmail.com>
2023-06-26 14:28:42 -0300

    fix: spigot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
     compileOnly("me.lucko:helper:5.6.10")
     compileOnly("com.github.PlaceholderAPI:PlaceholderAPI:2.10.9")
 
