981ba335f72a75d565df32497890508d480e3019
Davi Duarte <rio.daviduarte@gmail.com>
2023-06-26 13:57:12 -0300

    feat: clan model

---
+++ src/main/java/com/yuhtin/quotes/saint/leagues/model/LeagueClan.java
@@ -0,0 +1,20 @@
+package com.yuhtin.quotes.saint.leagues.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
+ */
+@RequiredArgsConstructor
+@Data
+public class LeagueClan {
+
+    private final String tag;
+    private int points;
+
+}
