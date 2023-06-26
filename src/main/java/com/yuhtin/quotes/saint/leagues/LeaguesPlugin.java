d9bc7c87f3bee9d66b6c1fbc20f0171e6fdd5d4a
Davi Duarte <rio.daviduarte@gmail.com>
2023-06-26 13:35:56 -0300

    feat: main class

---
+++ src/main/java/com/yuhtin/quotes/saint/leagues/LeaguesPlugin.java
@@ -0,0 +1,14 @@
+package com.yuhtin.quotes.saint.leagues;
+
+import me.lucko.helper.plugin.ExtendedJavaPlugin;
+
+/**
+ * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
+ */
+public class LeaguesPlugin extends ExtendedJavaPlugin {
+
+    @Override
+    protected void enable() {
+        getLogger().info("Plugin enabled!");
+    }
+}
