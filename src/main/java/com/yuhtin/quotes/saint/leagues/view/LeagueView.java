0b3dee9b401b79e64a84cdf7ba2c212b2b3568af
Davi Duarte <rio.daviduarte@gmail.com>
2023-06-28 15:04:32 -0300

    feat: league main view

---
+++ src/main/java/com/yuhtin/quotes/saint/leagues/view/LeagueView.java
@@ -0,0 +1,65 @@
+package com.yuhtin.quotes.saint.leagues.view;
+
+import com.henryfabio.minecraft.inventoryapi.editor.InventoryEditor;
+import com.henryfabio.minecraft.inventoryapi.inventory.impl.simple.SimpleInventory;
+import com.henryfabio.minecraft.inventoryapi.item.InventoryItem;
+import com.henryfabio.minecraft.inventoryapi.viewer.Viewer;
+import com.yuhtin.quotes.saint.leagues.LeaguesPlugin;
+import com.yuhtin.quotes.saint.leagues.cache.RankingCache;
+import com.yuhtin.quotes.saint.leagues.model.LeagueClan;
+import com.yuhtin.quotes.saint.leagues.model.LeagueEvent;
+import com.yuhtin.quotes.saint.leagues.util.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+/**
+ * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
+ */
+public class LeagueView extends SimpleInventory {
+
+    public LeagueView() {
+        super("league.main", "Liga", 3);
+    }
+
+    @Override
+    protected void configureInventory(Viewer viewer, InventoryEditor editor) {
+        Player player = viewer.getPlayer();
+
+        String clanTag = "TST";
+
+        LeagueClan clan = RankingCache.getInstance().getByTag(clanTag);
+        int rankingPosition = clan.getRankPosition();
+        int points = clan.getPoints();
+
+        int playerPoints = LeaguesPlugin.getInstance().getEventRepository()
+                .groupByPlayer(player.getName())
+                .stream()
+                .map(LeagueEvent::getPoints)
+                .reduce(0, Integer::sum);
+
+        editor.setItem(12, InventoryItem.of(new ItemBuilder(player.getName()).name("&aSeu Perfil")
+                .setLore(
+                        "&fSeu clan: &7" + clanTag + " &8| &a" + points + " pontos &6(#" + rankingPosition + ")",
+                        "&fSeus pontos : &a" + playerPoints,
+                        "",
+                        "&aClique para ver os eventos que participou!"
+                ).wrap()).defaultCallback());
+
+        editor.setItem(14, InventoryItem.of(new ItemBuilder(Material.NETHER_STAR).name("&aRanking da liga")
+                .setLore(
+                        "&7Veja quais clans estão liderando",
+                        "&7a liga atualmente",
+                        "",
+                        "&aClique para ver o ranking da liga!"
+                ).wrap()));
+
+        editor.setItem(15, InventoryItem.of(new ItemBuilder(Material.BLUE_BANNER).name("&aHistórico de Eventos")
+                .setLore(
+                        "&7Veja os eventos anteriores que",
+                        "&7ocorream na liga e quantos",
+                        "&7pontos foram fornecidos",
+                        "",
+                        "&aClique para ver o histórico!"
+                ).wrap()));
+    }
+}
