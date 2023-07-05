d241e80ed447051f8e917635acf71d9d095aed13
Davi Duarte <rio.daviduarte@gmail.com>
2023-07-05 18:08:05 -0300

    feat: add logs to hook module

--- src/main/java/com/yuhtin/quotes/saint/leagues/hook/HookModule.java
+++ src/main/java/com/yuhtin/quotes/saint/leagues/hook/HookModule.java
@@ -2,7 +2,7 @@ package com.yuhtin.quotes.saint.leagues.hook;
 
 import com.google.common.reflect.ClassPath;
 import com.yuhtin.quotes.saint.leagues.LeaguesPlugin;
-import com.yuhtin.quotes.saint.leagues.hook.impl.YClansHook;
+import lombok.AllArgsConstructor;
 import lombok.val;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
@@ -15,12 +15,13 @@ import java.io.IOException;
 /**
  * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
  */
+@AllArgsConstructor
 public class HookModule implements TerminableModule {
+
+    private final LeaguesPlugin instance;
 
     @Override
     public void setup(@Nonnull TerminableConsumer consumer) {
-
-        LeaguesPlugin plugin = LeaguesPlugin.getInstance();
         PluginManager pluginManager = Bukkit.getPluginManager();
 
         // get all classes that extends LeagueEventHook
@@ -30,32 +31,30 @@ public class HookModule implements TerminableModule {
         try {
             classPath = ClassPath.from(getClass().getClassLoader());
         } catch (IOException exception) {
-            plugin.getLogger().severe("ClassPath could not be instantiated");
+            instance.getLogger().severe("ClassPath could not be instantiated");
             return;
         }
 
         for (val info : classPath.getTopLevelClassesRecursive("com.yuhtin.quotes.saint.leagues.hook.impl")) {
             try {
-                val name = Class.forName(info.getName());
-                val object = name.newInstance();
-
-                if (object instanceof LeagueEventHook) {
-                    LeagueEventHook hook = (LeagueEventHook) object;
-                    plugin.getLogger().info("[" + hook.pluginName() + "] Iniciando hook...");
+                Class<?> name = Class.forName(info.getName());
+                if (name.newInstance() instanceof LeagueEventHook) {
+                    LeagueEventHook hook = (LeagueEventHook) name.getDeclaredConstructor(LeaguesPlugin.class).newInstance(instance);
+                    instance.getLogger().info("[" + hook.pluginName() + "] Iniciando hook...");
 
                     if (!pluginManager.isPluginEnabled(hook.pluginName())) {
-                        plugin.getLogger().warning("[" + hook.pluginName() + "] Dependencia " + hook.pluginName() + " não encontrada");
-                        plugin.getLogger().warning("[" + hook.pluginName() + "] Desabilitando hook deste plugin!");
+                        instance.getLogger().warning("[" + hook.pluginName() + "] Dependencia " + hook.pluginName() + " não encontrada");
+                        instance.getLogger().warning("[" + hook.pluginName() + "] Desabilitando hook deste plugin!");
 
                         continue;
                     }
 
-                    plugin.bindModule(hook);
-                    plugin.getLogger().info("[" + hook.pluginName() + "] Hook iniciado com sucesso!");
+                    instance.bindModule(hook);
+                    instance.getLogger().info("[" + hook.pluginName() + "] Hook iniciado com sucesso!");
                 } else throw new InstantiationException();
             } catch (Exception exception) {
                 exception.printStackTrace();
-                plugin.getLogger().severe("The " + info.getName() + " class could not be instantiated");
+                instance.getLogger().severe("The " + info.getName() + " class could not be instantiated");
             }
         }
 
