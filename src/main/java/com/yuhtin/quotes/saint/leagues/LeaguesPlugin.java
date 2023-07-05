42075cf5ec85d1dd44823ca43faffd8e8f81cc1c
Davi Duarte <rio.daviduarte@gmail.com>
2023-07-05 18:08:52 -0300

    feat: call & bind modules

--- src/main/java/com/yuhtin/quotes/saint/leagues/LeaguesPlugin.java
+++ src/main/java/com/yuhtin/quotes/saint/leagues/LeaguesPlugin.java
@@ -1,12 +1,20 @@
 package com.yuhtin.quotes.saint.leagues;
 
+import com.henryfabio.minecraft.inventoryapi.manager.InventoryManager;
 import com.henryfabio.sqlprovider.connector.SQLConnector;
 import com.henryfabio.sqlprovider.executor.SQLExecutor;
+import com.yuhtin.quotes.saint.leagues.cache.ViewCache;
+import com.yuhtin.quotes.saint.leagues.command.LeagueClanCommand;
+import com.yuhtin.quotes.saint.leagues.hook.HookModule;
+import com.yuhtin.quotes.saint.leagues.manager.SimpleClansAccessor;
+import com.yuhtin.quotes.saint.leagues.module.AutoRewardModule;
+import com.yuhtin.quotes.saint.leagues.module.RankingModule;
 import com.yuhtin.quotes.saint.leagues.repository.SQLProvider;
 import com.yuhtin.quotes.saint.leagues.repository.repository.ClanRepository;
 import com.yuhtin.quotes.saint.leagues.repository.repository.EventRepository;
 import lombok.Getter;
 import me.lucko.helper.plugin.ExtendedJavaPlugin;
+import org.bukkit.Bukkit;
 
 /**
  * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
@@ -14,14 +22,39 @@ import me.lucko.helper.plugin.ExtendedJavaPlugin;
 @Getter
 public class LeaguesPlugin extends ExtendedJavaPlugin {
 
+    private final SimpleClansAccessor simpleClansAccessor = new SimpleClansAccessor(Bukkit.getPluginManager());
+    private final ViewCache viewCache = new ViewCache();
+
+    private RankingModule rankingModule;
     private ClanRepository clanRepository;
     private EventRepository eventRepository;
 
     @Override
     protected void enable() {
+        saveDefaultConfig();
+
+        if (!simpleClansAccessor.isValid()) {
+            getLogger().severe("SimpleClans not found! Disabling plugin...");
+            return;
+        }
+
+        InventoryManager.enable(this);
+
         initDatabase();
 
+        bindModule(new AutoRewardModule(this));
+        bindModule(new LeagueClanCommand(this));
+        bindModule(new HookModule(this));
+
+        this.rankingModule = new RankingModule(this);
+        bindModule(rankingModule);
+
         getLogger().info("Plugin enabled!");
+    }
+
+    @Override
+    protected void disable() {
+        rankingModule.clearStands();
     }
 
     private void initDatabase() {
@@ -30,6 +63,9 @@ public class LeaguesPlugin extends ExtendedJavaPlugin {
 
         clanRepository = new ClanRepository(sqlExecutor);
         eventRepository = new EventRepository(sqlExecutor);
+
+        clanRepository.createTable();
+        eventRepository.createTable();
 
         getLogger().info("Database initialized!");
     }
