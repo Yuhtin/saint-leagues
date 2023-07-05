56ef358d4375346f3462ea25d7bb923de107998e
Davi Duarte <rio.daviduarte@gmail.com>
2023-07-05 18:07:54 -0300

    fix: set ranking location

--- src/main/java/com/yuhtin/quotes/saint/leagues/command/LeagueClanCommand.java
+++ src/main/java/com/yuhtin/quotes/saint/leagues/command/LeagueClanCommand.java
@@ -1,8 +1,8 @@
 package com.yuhtin.quotes.saint.leagues.command;
 
 import com.yuhtin.quotes.saint.leagues.LeaguesPlugin;
-import com.yuhtin.quotes.saint.leagues.model.LeagueClan;
-import com.yuhtin.quotes.saint.leagues.repository.repository.ClanRepository;
+import com.yuhtin.quotes.saint.leagues.cache.LeagueClanCache;
+import lombok.AllArgsConstructor;
 import me.lucko.helper.Commands;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
@@ -10,48 +10,63 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
-import java.util.Optional;
 
 /**
  * @author <a href="https://github.com/Yuhtin">Yuhtin</a>
  */
+@AllArgsConstructor
 public class LeagueClanCommand implements TerminableModule {
+
+    private final LeaguesPlugin instance;
 
     @Override
     public void setup(@Nonnull TerminableConsumer consumer) {
 
-        Commands.parserRegistry().register(LeagueClan.class, context -> {
-            LeagueClan clan = LeaguesPlugin.getInstance().getClanRepository().findByTag(context);
-            if (clan == null) return Optional.empty();
-
-            return Optional.of(clan);
-        });
+        Commands.create()
+                .assertPlayer()
+                .handler(context -> {
+                    instance.getViewCache().getLeagueView().openInventory(context.sender());
+                }).registerAndBind(consumer, "liga");
 
         Commands.create()
                 .assertPermission("league.admin")
                 .assertUsage("<add/remove> <clan> <pontos>")
                 .handler(context -> {
                     String action = context.arg(0).parseOrFail(String.class);
-                    LeagueClan clan = context.arg(1).parseOrFail(LeagueClan.class);
-                    int points = context.arg(2).parseOrFail(Integer.class);
+                    String clanTag = context.arg(1).parseOrFail(String.class).toUpperCase();
+                    int points = Math.max(0, context.arg(2).parseOrFail(Integer.class));
 
-                    ClanRepository clanRepository = LeaguesPlugin.getInstance().getClanRepository();
-                    if (action.equalsIgnoreCase("add")) {
-                        clan.setPoints(clan.getPoints() + points);
-                        clanRepository.insert(clan);
-                    } else {
-                        clan.setPoints(Math.max(0, clan.getPoints() - points));
-                        clanRepository.insert(clan);
+                    int currentPoints = LeagueClanCache.getInstance().getPointsByTag(clanTag);
+                    if (currentPoints == -1) {
+                        context.reply("&cClan não encontrado!");
+                        return;
                     }
 
-                    context.reply("&aClan &f" + clan.getTag() + " &aagora possui &f" + clan.getPoints() + " &apontos!");
-                }).registerAndBind(consumer, "liga");
+                    if (action.equalsIgnoreCase("add")) {
+                        currentPoints += points;
+                    } else {
+                        currentPoints -= points;
+                    }
+
+                    currentPoints = Math.max(currentPoints, 0);
+                    LeagueClanCache.getInstance().setClanPoints(clanTag, currentPoints);
+
+                    context.reply("&aClan &f" + clanTag + " &aagora possui &f" + currentPoints + " &apontos!");
+                }).registerAndBind(consumer, "ligaadm");
 
         Commands.create()
                 .assertPlayer()
                 .assertPermission("league.admin")
                 .assertUsage("<posição>")
                 .handler(context -> {
+                    String action = context.arg(0).parseOrFail(String.class);
+                    if (action.equalsIgnoreCase("reload")) {
+                        instance.getRankingModule().refreshRanking();
+
+                        context.reply("&aRanking recarregado!");
+                        return;
+                    }
+
                     int position = context.arg(0).parseOrFail(Integer.class);
                     if (position < 1 || position > 10) {
                         context.reply("&cPosição inválida! &8(Apenas de 1 à 10)");
@@ -62,14 +77,14 @@ public class LeagueClanCommand implements TerminableModule {
 
                     Location location = player.getLocation();
                     String locationString = location.getWorld().getName() + ","
-                            + location.getBlockX() + ","
-                            + location.getBlockY() + ","
-                            + location.getBlockZ() + ","
+                            + location.getX() + ","
+                            + location.getY() + ","
+                            + location.getZ() + ","
                             + location.getYaw() + ","
                             + location.getPitch();
 
-                    LeaguesPlugin.getInstance().getConfig().set("ranking.position." + position, locationString);
-                    LeaguesPlugin.getInstance().saveConfig();
+                    instance.getConfig().set("ranking.position." + position, locationString);
+                    instance.saveConfig();
 
                     context.reply("&aVocê definiu a posição " + position + " do ranking!");
                 }).registerAndBind(consumer, "ligasetranking");
